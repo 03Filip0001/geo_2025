02b07039056ee12adc6f1628cf1d59f44ed88c7c
03Filip0001 <hackboyiv@gmail.com>
2025-10-06 13:29:16 +0200

    Ucitana i prikazana mapa sa opstinama Srbije

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+
 print("\n\nPROJEKTNI ZADATAK 9 (GEO 1)\n####################")
+
+# Loading data from .shp file (adm1 - opstine Srbije)
+try:
+	opstine_gdf = gpd.read_file("data/SRB_adm1.shp")
+	print("Shapefile uspešno učitan.")
+except:
+	raise Exception("Cannot load data")
+
+opstine_gdf.plot()
+plt.show()
